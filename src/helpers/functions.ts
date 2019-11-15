cd1157da8699d094e08d68f575308a62b02b0e7f
Aryan Alikhani <collaborativeroom@gmail.com>
2019-11-15 22:32:40 +0330

    exposed xrange

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -2,7 +2,7 @@ const s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 export const range = (size: number) => Array.apply(null, Array(size)).map((a, i) => i);
 export const step = (x: number) => x >= 0 ? 1 : 0;
 export const ramp = (x: number) => +x * step(x);
-const xrange = (start: number, size: number) => Array.apply(null, Array(~~(size - start + 1))).map((_, j) => j + Math.floor(start));
+export const xrange = (start: number, size: number) => Array.apply(null, Array(~~(size - start + 1))).map((_, j) => j + Math.floor(start));
 
 export function optional<T>(fn: () => T, def?: T): T {
   try {
@@ -15,13 +15,12 @@ export function optional<T>(fn: () => T, def?: T): T {
   }
 }
 
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
 export function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
     s4() + '-' + s4() + s4() + s4();
 }
