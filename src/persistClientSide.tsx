ee7023f7213f0f284af90f7bc98164d06459cc20
Aryan <collaborativeroom@gmail.com>
2020-01-14 16:13:14 +0330

    3.3.8

--- src/persistClientSide.tsx
+++ src/persistClientSide.tsx
@@ -51,28 +51,6 @@ export const restorePersistedDataOnClientSide = (context: RequestContext) => {
   if (typeof window.localStorage != undefined) {
     const versionKey = `${context.storagePrefix}_version`;
     const version = localStorage.getItem(versionKey) || 1;
-
-    if (version != context.version) {
-      context.services.forEach((service) => {
-        const {id} = metadataOf(service);
-        try {
-          const key = `${context.storagePrefix}_bridge${id}`;
-          if(service.migrate) {
-            const data = localStorage.getItem(key);
-            if (data) {
-              const content = clientDecrypt(data, key, context.encrypt);
-              const json = JSON.parse(content);
-              service.migrate.apply(service, json, version, context.version)
-            }
-          }
-          localStorage.removeItem(key)
-        }catch (e) {
-          console.error(e);
-        }
-      });
-      localStorage.setItem(versionKey, context.version.toString());
-      return
-    }
     context.services.forEach((service) => {
       const {id, persist = []} = metadataOf(service);
       if (persist.length > 0) {
@@ -82,17 +60,24 @@ export const restorePersistedDataOnClientSide = (context: RequestContext) => {
           if (data) {
             const content = clientDecrypt(data, key, context.encrypt);
             const json = JSON.parse(content);
-            persist.forEach((data: any) => {
-              const {key} = data;
-              if (typeof json[key] !== 'undefined' && json[key] !== null) {
-                service[key] = json[key];
-              }
-            });
+            if (version != context.version && service.migrate) {
+                service.migrate.apply(service, json, version, context.version)
+            }else {
+              persist.forEach((data: any) => {
+                const {key} = data;
+                if (typeof json[key] !== 'undefined' && json[key] !== null) {
+                  service[key] = json[key];
+                }
+              });
+            }
           }
         } catch (e) {
           console.error(e);
         }
       }
     });
+    if (version != context.version) {
+      localStorage.setItem(versionKey, context.version.toString());
+    }
   }
 };
